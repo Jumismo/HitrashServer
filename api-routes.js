445a1c53bcc57ae1c23eaa53233e350f1cb30437
Juan Miguel Sánchez Begines <jumismo@uoc.edu>
2019-11-08 21:06:54 +0100

    Fix error syntax

--- api-routes.js
+++ api-routes.js
@@ -4,6 +4,8 @@ var options = {
      setHeaders: function (res, path, stat) {
         res.set('Access-Control-Allow-Origin', '*');
         res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    }
+};
 
 let router = require('express').Router([options]);
 
